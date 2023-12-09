a6a6bbc1e6ee913ff6da74d6121cdee21a51bcb7
alfianlion <alfianlion@gmail.com>
2023-12-09 16:11:39 +0800

    App is now using router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from 'src/page/home/home.component';
+import { NotFoundComponent } from 'src/page/not-found/not-found.component';
+import { WorksComponent } from 'src/page/works/works.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'works', component: WorksComponent },
+  { path: '**', component: NotFoundComponent },  // Wildcard route for a 404 page
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from 'src/page/home/home.component';
+import { WorksComponent } from 'src/page/works/works.component';
+import { HeaderComponent } from 'src/components/header/header.component';
+import { FooterComponent } from 'src/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorksComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
