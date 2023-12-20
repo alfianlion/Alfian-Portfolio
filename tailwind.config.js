ae9aa5589719c7bd15b7f6d1ae32ecb9434048f4
alfianlion <alfianlion@gmail.com>
2023-12-20 16:48:36 +0800

    Added tailwind keyframes to animate navigation and tailwind containerize contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slidein: {
+          "0%": { opacity: "0", transform: "translateY(60px)" },
+          "100%": { opacity: "100", transform: "translateY(0px)" },
+        },
+      },
+      animation: {
+        'sliding-in': 'slidein 1s linear',
+      },
+    },
   },
   plugins: [],
-}
+};
