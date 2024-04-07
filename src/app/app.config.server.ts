047c7651dadc576e4cabfd89a42a4e002ce6738c
alfianlion <alfianlion@gmail.com>
2024-04-07 15:05:07 +0800

    Working on the Header

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -5,10 +5,12 @@ import {
 } from '@angular/platform-server';
 
 import { appConfig } from './app.config';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
+    provideNoopAnimations(),
     { provide: SERVER_CONTEXT, useValue: 'ssr-analog' },
   ],
 };
---
+++ src/app/components/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './(layout)/header.component.html',
+})
+export default class HeaderComponent {
+}
